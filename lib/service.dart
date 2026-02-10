a691e92f160f788a3c03407d5683c5637c23d9e3
rishannk00077-cmd <rishannk00077@gmail.com>
2026-02-10 11:29:25 +0530

    edit

--- lib/service.dart
+++ lib/service.dart
@@ -1,8 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-Future<void> addStudent(String Name,String Email,String Course,BuildContext context)async {
-  await FirebaseFirestore.instance.collection("students").add({"NAME":Name,"EMAIL":Email,"COURSE":Course});
-  Navigator.pop(context);
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Student Created successfuly")));
+Future<void> addStudent(
+  String Name,
+  String Email,
+  String Course,
+  BuildContext context,
+) async {
+  await FirebaseFirestore.instance.collection("students").add({
+    "NAME": Name,
+    "EMAIL": Email,
+    "COURSE": Course,
+  });
+ 
+  ScaffoldMessenger.of(
+    context,
+  ).showSnackBar(SnackBar(content: Text("Student Created successfuly")));
+}
+Stream<QuerySnapshot>getstudents(){
+  return FirebaseFirestore.instance.collection("students").snapshots();
+}
+Future<void>deletestudent(
+  String id,BuildContext context,
+)async{
+  await FirebaseFirestore.instance.collection("students").doc(id).delete();
+  ScaffoldMessenger.of(context,).showSnackBar(SnackBar(content: Text("Deleted Successfully")));
+}
+Future<void>updatestudent(
+  String id,Name,Email,Course,BuildContext context,
+)async{
+  await FirebaseFirestore.instance.collection("students").doc(id).update({
+    "NAME": Name,
+    "EMAIL": Email,
+    "COURSE": Course,
+  });
 }
--- lib/std_directory.dart
+++ lib/std_directory.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:student_directory_app/service.dart';
 
 class StdDirectory extends StatefulWidget {
   const StdDirectory({super.key});
@@ -8,9 +10,64 @@ class StdDirectory extends StatefulWidget {
 }
 
 class _StdDirectoryState extends State<StdDirectory> {
-  TextEditingController Namec=TextEditingController();
-  TextEditingController Emailc=TextEditingController();
-  TextEditingController Coursec=TextEditingController();
+  TextEditingController Namec = TextEditingController();
+  TextEditingController Emailc = TextEditingController();
+  TextEditingController Coursec = TextEditingController();
+  void editbox(DocumentSnapshot doc) {
+    Namec.text = doc["NAME"];
+    Emailc.text = doc["EMAIL"];
+    Coursec.text = doc["COURSE"];
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        children: [
+          Text("Edit Student",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+          SizedBox(height: 20,),
+          TextField(
+            controller: Namec,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(20)),
+              fillColor: Colors.amber,
+              hintText: "Name",
+            ),
+          ),
+          SizedBox(height: 20,),
+          TextField(
+            controller: Emailc,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(20)),
+              fillColor: Colors.amber,
+              hintText: "Email",
+            ),
+          ),
+          SizedBox(height: 20,),
+          TextField(
+            controller: Coursec,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(20)),
+              fillColor: Colors.amber,
+              hintText: "Course",
+            ),
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            style: ButtonStyle(),
+            onPressed: () {
+              updatestudent(
+                doc.id,
+                Namec.text,
+                Emailc.text,
+                Coursec.text,
+                context,
+              );
+            },
+            child: Text("Edit",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,25 +94,29 @@ class _StdDirectoryState extends State<StdDirectory> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                content: Column(mainAxisSize: MainAxisSize.min,
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    TextField(controller: Namec,
+                    TextField(
+                      controller: Namec,
                       decoration: InputDecoration(
-                        fillColor: Colors.white,
+                        fillColor: Colors.amber,
                         filled: true,
                         hintText: "Name",
                       ),
                     ),
-                    SizedBox(height: 10,),
-                    TextField(controller: Emailc,
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: Emailc,
                       decoration: InputDecoration(
                         fillColor: Colors.white,
                         filled: true,
                         hintText: "Email",
                       ),
                     ),
-                    SizedBox(height: 10,),
-                    TextField(controller: Coursec,
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: Coursec,
                       decoration: InputDecoration(
                         fillColor: Colors.white,
                         filled: true,
@@ -68,7 +129,14 @@ class _StdDirectoryState extends State<StdDirectory> {
                         backgroundColor: Colors.blue,
                         foregroundColor: Colors.black,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        addStudent(
+                          Namec.text,
+                          Emailc.text,
+                          Coursec.text,
+                          context,
+                        );
+                      },
                       child: Text("Add"),
                     ),
                   ],
@@ -80,6 +148,48 @@ class _StdDirectoryState extends State<StdDirectory> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         child: Icon(Icons.add),
+      ),
+      body: StreamBuilder(
+        stream: getstudents(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final data = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(data[index]["NAME"]),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(data[index]["EMAIL"]),
+                    Text(data[index]["COURSE"]),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        deletestudent(data[index].id, context);
+                      },
+                      icon: Icon(Icons.delete, color: Colors.red),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        editbox(data[index]);
+                      },
+                      icon: Icon(Icons.edit, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
